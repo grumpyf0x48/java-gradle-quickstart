ef34a8e36057b9ccbe797c5ebe9b8d3328ab41be
Pierre-Yves Fourmond <pyfourmond@gmail.com>
2022-10-08 18:39:29 +0200

    Initial import

---
+++ src/main/java/org/grumpyf0x48/pkg/App.java
@@ -0,0 +1,12 @@
+package org.grumpyf0x48.pkg;
+
+public class App {
+
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+    }
+}
