3e85b1b5f27d00a43c921c8251dc3d739b55f236
Pierre-Yves Fourmond <pyfourmond@gmail.com>
2022-10-09 08:58:41 +0200

    Add .editorconfig for code formatting

--- src/test/java/org/grumpyf0x48/pkg/AppTest.java
+++ src/test/java/org/grumpyf0x48/pkg/AppTest.java
@@ -1,11 +1,13 @@
 package org.grumpyf0x48.pkg;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class AppTest {
 
-    @Test void appHasAGreeting() {
+    @Test
+    void appHasAGreeting() {
         App app = new App();
         assertNotNull(app.getGreeting(), "app should have a greeting");
     }
