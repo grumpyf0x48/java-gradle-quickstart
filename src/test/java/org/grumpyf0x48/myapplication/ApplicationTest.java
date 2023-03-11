c02c2622c9e4f3bdf2b5df81547300aebc0b565d
Pierre-Yves Fourmond <pyfourmond@gmail.com>
2023-03-11 13:34:51 +0100

    Missing public keyword

--- src/test/java/org/grumpyf0x48/myapplication/ApplicationTest.java
+++ src/test/java/org/grumpyf0x48/myapplication/ApplicationTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-class ApplicationTest {
+public class ApplicationTest {
 
     @Test
     void appHasAGreeting() {
