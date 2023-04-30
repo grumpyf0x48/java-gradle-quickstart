532a10781e19df8715670e6e8fa38e3d0002334d
Pierre-Yves Fourmond <33195569+grumpyf0x48@users.noreply.github.com>
2023-04-30 16:53:39 +0200

    Rename default application to 'java_gradle_quickstart' (#22)

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ dependencies {
 }
 
 application {
-    mainClass.set("org.grumpyf0x48.myapplication.Application")
+    mainClass.set("org.grumpyf0x48.java_gradle_quickstart.Application")
 }
 
 java {
@@ -27,7 +27,7 @@ distributions {
     create("native") {
         contents {
             from("${project.buildDir}/native/nativeCompile") {
-                include("myapplication")
+                include("java_gradle_quickstart")
                 into("bin")
             }
         }
@@ -53,7 +53,7 @@ graalvmNative {
             resources.autodetect()
         }
         named("main") {
-            imageName.set("myapplication")
+            imageName.set("java_gradle_quickstart")
         }
     }
 }
--- src/test/java/org/grumpyf0x48/java_gradle_quickstart/ApplicationTest.java
+++ src/test/java/org/grumpyf0x48/java_gradle_quickstart/ApplicationTest.java
@@ -1,4 +1,4 @@
-package org.grumpyf0x48.myapplication;
+package org.grumpyf0x48.java_gradle_quickstart;
 
 import org.junit.jupiter.api.Test;
 
